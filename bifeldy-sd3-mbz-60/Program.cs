c112a023df59628dff57b013473eab1db90c5734
B. Bias A. Ch <bias@indomaret.co.id>
2023-12-21 14:58:30 +0700

    Fix Async ~

--- bifeldy-sd3-mbz-60/Program.cs
+++ bifeldy-sd3-mbz-60/Program.cs
@@ -40,7 +40,7 @@ Bifeldy.AddSwagger(
 Bifeldy.SetupDI();
 
 // Background Hosted Service Long Run Task Di Sini --
-Bifeldy.AddKafkaConsumerBackground("172.31.2.122:1234", "pbro_web_test");
+Bifeldy.AddKafkaConsumerBackground("172.31.2.122:9092", "pbro_web_test");
 
 builder.Services.AddCors();
 builder.Services.AddControllers(x => {
