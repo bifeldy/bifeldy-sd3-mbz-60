dc077ad64797047f8fd71324164d4b8319ae005b
B. Bias A. Ch <bias@indomaret.co.id>
2024-01-09 19:23:32 +0700

    Dynamic Kode Dc ~

--- bifeldy-sd3-mbz-60/Program.cs
+++ bifeldy-sd3-mbz-60/Program.cs
@@ -40,7 +40,7 @@ Bifeldy.AddSwagger(
 Bifeldy.SetupDI();
 
 // Background Hosted Service Long Run Task Di Sini --
-Bifeldy.AddKafkaConsumerBackground("172.31.2.122:9092", "web_approval_G001");
+Bifeldy.AddKafkaConsumerBackground("172.31.2.122:9092", "web_approval", suffixKodeDc: true);
 
 builder.Services.AddCors();
 builder.Services.AddControllers(x => {
